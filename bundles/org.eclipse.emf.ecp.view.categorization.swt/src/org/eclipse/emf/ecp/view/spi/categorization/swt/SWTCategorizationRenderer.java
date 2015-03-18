50fe811d559f8d00970766edb9107d19317a720a
eneufeld <neufeld.eugen@googlemail.com>
2015-03-18 14:51:09 +0100

    Bug 461484 - [ViewModel] Internationalize strings used by renderers https://bugs.eclipse.org/bugs/show_bug.cgi?id=461484 updated categorization renderer dependency

--- bundles/org.eclipse.emf.ecp.view.categorization.swt/src/org/eclipse/emf/ecp/view/spi/categorization/swt/SWTCategorizationRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.categorization.swt/src/org/eclipse/emf/ecp/view/spi/categorization/swt/SWTCategorizationRenderer.java
@@ -12,8 +12,12 @@
  ******************************************************************************/
 package org.eclipse.emf.ecp.view.spi.categorization.swt;
 
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.emf.databinding.EMFDataBindingContext;
+import org.eclipse.emf.databinding.edit.EMFEditObservables;
 import org.eclipse.emf.ecp.view.internal.categorization.swt.MessageKeys;
 import org.eclipse.emf.ecp.view.spi.categorization.model.VCategorization;
+import org.eclipse.emf.ecp.view.spi.model.VViewPackage;
 import org.eclipse.emf.ecp.view.spi.renderer.NoPropertyDescriptorFoundExeption;
 import org.eclipse.emf.ecp.view.spi.renderer.NoRendererFoundException;
 import org.eclipse.emf.ecp.view.spi.swt.AbstractSWTRenderer;
@@ -22,7 +26,9 @@ import org.eclipse.emf.ecp.view.spi.swt.layout.GridDescriptionFactory;
 import org.eclipse.emf.ecp.view.spi.swt.layout.LayoutProviderHelper;
 import org.eclipse.emf.ecp.view.spi.swt.layout.SWTGridCell;
 import org.eclipse.emf.ecp.view.spi.swt.layout.SWTGridDescription;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.emfforms.spi.localization.LocalizationServiceHelper;
+import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -37,12 +43,14 @@ import org.eclipse.swt.widgets.Label;
  */
 public class SWTCategorizationRenderer extends AbstractSWTRenderer<VCategorization> {
 	private SWTGridDescription rendererGridDescription;
+	private final EMFDataBindingContext dataBindingContext;
 
 	/**
 	 * Default constructor.
 	 */
 	public SWTCategorizationRenderer() {
 		super();
+		dataBindingContext = new EMFDataBindingContext();
 	}
 
 	/**
@@ -52,6 +60,7 @@ public class SWTCategorizationRenderer extends AbstractSWTRenderer<VCategorizati
 	 */
 	SWTCategorizationRenderer(SWTRendererFactory factory) {
 		super(factory);
+		dataBindingContext = new EMFDataBindingContext();
 	}
 
 	/**
@@ -62,6 +71,7 @@ public class SWTCategorizationRenderer extends AbstractSWTRenderer<VCategorizati
 	@Override
 	protected void dispose() {
 		rendererGridDescription = null;
+		dataBindingContext.dispose();
 		super.dispose();
 	}
 
@@ -97,8 +107,13 @@ public class SWTCategorizationRenderer extends AbstractSWTRenderer<VCategorizati
 		headingLbl.setData(CUSTOM_VARIANT, "org_eclipse_emf_ecp_categorization_title"); //$NON-NLS-1$
 		final Label whatToDoLbl = new Label(categoryComposite, SWT.NONE);
 		whatToDoLbl.setData(CUSTOM_VARIANT, "org_eclipse_emf_ecp_categorization_message"); //$NON-NLS-1$
-		final String headingText = getVElement().getName();
-		headingLbl.setText(headingText == null ? "" : headingText); //$NON-NLS-1$
+
+		final IObservableValue modelValue = EMFEditObservables.observeValue(
+			AdapterFactoryEditingDomain.getEditingDomainFor(getVElement()), getVElement(),
+			VViewPackage.eINSTANCE.getElement_Label());
+		final IObservableValue targetValue = SWTObservables.observeText(headingLbl);
+		dataBindingContext.bindValue(targetValue, modelValue);
+
 		whatToDoLbl.setText(LocalizationServiceHelper.getString(getClass(), MessageKeys.Categorization_Selection));
 		return categoryComposite;
 	}
