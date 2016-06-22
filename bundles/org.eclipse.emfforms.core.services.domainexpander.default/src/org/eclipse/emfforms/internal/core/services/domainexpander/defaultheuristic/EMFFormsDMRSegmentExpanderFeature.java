5115b9c59591e61304dc3c38fec387c410d650b9
Lucas Koehler <l.koehler93@gmail.com>
2016-06-22 17:02:29 +0200

    Bug 462873 - Change DMR to use segments
    EMFFormsDomainExpander and feature expander:
    
    * Adjusted EMFFormsDomainExpanderImpl to DMR segments.
    * Defined EMFFormsDMRSegmentExpander interface
    * Implemented EMFFormsDMRSegmentExpanderFeature for FeatureDMRSegments
    * Deleted obsolete EMFFormsDMRExpander interface
    
    Change-Id: I7011483d6b1a7b76894b598e9c5cffab2d3ce00c
    Signed-off-by: Lucas Koehler <l.koehler93@gmail.com>

---
+++ bundles/org.eclipse.emfforms.core.services.domainexpander.default/src/org/eclipse/emfforms/internal/core/services/domainexpander/defaultheuristic/EMFFormsDMRSegmentExpanderFeature.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2016 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Lucas Koehler - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emfforms.internal.core.services.domainexpander.defaultheuristic;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecp.common.spi.asserts.Assert;
+import org.eclipse.emf.ecp.view.spi.model.VDomainModelReferenceSegment;
+import org.eclipse.emf.ecp.view.spi.model.VFeatureDomainModelReferenceSegment;
+import org.eclipse.emfforms.spi.common.report.AbstractReport;
+import org.eclipse.emfforms.spi.common.report.ReportService;
+import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRSegmentExpander;
+import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsExpandingFailedException;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * A {@link EMFFormsDMRSegmentExpander} for {@link VFeatureDomainModelReferenceSegment
+ * VFeatureDomainModelReferenceSegments}.
+ *
+ * @author Lucas Koehler
+ *
+ */
+@Component(name = "EMFFormsDMRSegmentExpanderFeature")
+public class EMFFormsDMRSegmentExpanderFeature implements EMFFormsDMRSegmentExpander {
+
+	private ReportService reportService;
+
+	/**
+	 * Called by the framework to set the {@link ReportService}.
+	 *
+	 * @param reportService The {@link ReportService}
+	 */
+	@Reference
+	protected void setReportService(ReportService reportService) {
+		this.reportService = reportService;
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRSegmentExpander#prepareDomainObject(org.eclipse.emf.ecp.view.spi.model.VDomainModelReferenceSegment,
+	 *      org.eclipse.emf.ecore.EObject)
+	 */
+	@Override
+	public EObject prepareDomainObject(VDomainModelReferenceSegment segment, EObject domainObject)
+		throws EMFFormsExpandingFailedException {
+		Assert.create(segment).notNull();
+		Assert.create(domainObject).notNull();
+		Assert.create(segment).ofClass(VFeatureDomainModelReferenceSegment.class);
+
+		final VFeatureDomainModelReferenceSegment featureSegment = (VFeatureDomainModelReferenceSegment) segment;
+		final EStructuralFeature structuralFeature = domainObject.eClass()
+			.getEStructuralFeature(featureSegment.getDomainModelFeature());
+
+		if (structuralFeature == null) {
+			throw new EMFFormsExpandingFailedException(
+				String.format("The given domain object does not contain the segment's feature. " //$NON-NLS-1$
+					+ "The segment was %1$s. The domain object was %2$s.", segment, domainObject)); //$NON-NLS-1$
+		}
+		if (!EReference.class.isInstance(structuralFeature)) {
+			throw new EMFFormsExpandingFailedException(
+				String.format("The feature described by the given segment must be an EReference. " //$NON-NLS-1$
+					+ "The segment was %1$s.", segment)); //$NON-NLS-1$
+		}
+
+		final EReference reference = (EReference) structuralFeature;
+
+		/*
+		 * If the reference's target already exists, we do not need to do anything.
+		 * Otherwise we have to create a new instance.
+		 */
+		EObject child = (EObject) domainObject.eGet(reference);
+		if (child == null) {
+			if (!reference.getEReferenceType().isAbstract() && !reference.getEReferenceType().isInterface()) {
+				child = EcoreUtil.create(reference.getEReferenceType());
+				domainObject.eSet(reference, child);
+			} else {
+				throw new EMFFormsExpandingFailedException(String.format(
+					"The reference type of the segment's feature is either abstract or an interface. " //$NON-NLS-1$
+						+ "Therefore, no instance can be created. The segment was %1$s.", //$NON-NLS-1$
+					segment));
+			}
+		}
+
+		return child;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRSegmentExpander#isApplicable(org.eclipse.emf.ecp.view.spi.model.VDomainModelReferenceSegment)
+	 */
+	@Override
+	public double isApplicable(VDomainModelReferenceSegment segment) {
+		if (segment == null) {
+			reportService.report(new AbstractReport("Warning: The given domain model reference segment was null.")); //$NON-NLS-1$
+			return NOT_APPLICABLE;
+		}
+		if (VFeatureDomainModelReferenceSegment.class.isInstance(segment)) {
+			return 1d;
+		}
+		return NOT_APPLICABLE;
+	}
+
+}
--- bundles/org.eclipse.emfforms.core.services.domainexpander.default/src/org/eclipse/emfforms/internal/core/services/domainexpander/defaultheuristic/EMFFormsDomainExpanderImpl.java
+++ bundles/org.eclipse.emfforms.core.services.domainexpander.default/src/org/eclipse/emfforms/internal/core/services/domainexpander/defaultheuristic/EMFFormsDomainExpanderImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ * Copyright (c) 2011-2016 EclipseSource Muenchen GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,16 +8,19 @@
  *
  * Contributors:
  * Lucas Koehler - initial API and implementation
+ * Lucas Koehler - adjusted to DMR Segments
  ******************************************************************************/
 package org.eclipse.emfforms.internal.core.services.domainexpander.defaultheuristic;
 
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecp.common.spi.asserts.Assert;
 import org.eclipse.emf.ecp.view.spi.model.VDomainModelReference;
-import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRExpander;
+import org.eclipse.emf.ecp.view.spi.model.VDomainModelReferenceSegment;
+import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRSegmentExpander;
 import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDomainExpander;
 import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsExpandingFailedException;
 import org.osgi.service.component.annotations.Component;
@@ -33,25 +36,25 @@ import org.osgi.service.component.annotations.ReferencePolicy;
  */
 @Component(name = "EMFFormsDomainExpanderImpl")
 public class EMFFormsDomainExpanderImpl implements EMFFormsDomainExpander {
-	private final Set<EMFFormsDMRExpander> emfFormsDMRExpanders = new CopyOnWriteArraySet<EMFFormsDMRExpander>();
+	private final Set<EMFFormsDMRSegmentExpander> emfFormsDMRSegmentExpanders = new CopyOnWriteArraySet<EMFFormsDMRSegmentExpander>();
 
 	/**
-	 * Called by the framework to add an {@link EMFFormsDMRExpander} to the set of DMR expanders.
+	 * Called by the framework to add an {@link EMFFormsDMRSegmentExpander} to the set of DMR segment expanders.
 	 *
-	 * @param emfFormsDMRExpander The {@link EMFFormsDMRExpander} to add
+	 * @param emfFormsDMRSegmentExpander The {@link EMFFormsDMRSegmentExpander} to add
 	 */
 	@Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
-	protected void addEMFFormsDMRExpander(EMFFormsDMRExpander emfFormsDMRExpander) {
-		emfFormsDMRExpanders.add(emfFormsDMRExpander);
+	protected void addEMFFormsDMRSegmentExpander(EMFFormsDMRSegmentExpander emfFormsDMRSegmentExpander) {
+		emfFormsDMRSegmentExpanders.add(emfFormsDMRSegmentExpander);
 	}
 
 	/**
-	 * Called by the framework to remove an {@link EMFFormsDMRExpander} from the set of DMR expanders.
+	 * Called by the framework to remove an {@link EMFFormsDMRSegmentExpander} from the set of DMR segment expanders.
 	 *
-	 * @param emfFormsDMRExpander The {@link EMFFormsDMRExpander} to remove
+	 * @param emfFormsDMRSegmentExpander The {@link EMFFormsDMRSegmentExpander} to remove
 	 */
-	protected void removeEMFFormsDMRExpander(EMFFormsDMRExpander emfFormsDMRExpander) {
-		emfFormsDMRExpanders.remove(emfFormsDMRExpander);
+	protected void removeEMFFormsDMRSegmentExpander(EMFFormsDMRSegmentExpander emfFormsDMRSegmentExpander) {
+		emfFormsDMRSegmentExpanders.remove(emfFormsDMRSegmentExpander);
 	}
 
 	/**
@@ -66,20 +69,43 @@ public class EMFFormsDomainExpanderImpl implements EMFFormsDomainExpander {
 		Assert.create(domainModelReference).notNull();
 		Assert.create(domainObject).notNull();
 
-		EMFFormsDMRExpander bestDMRExpander = null;
-		double bestScore = Double.NEGATIVE_INFINITY;
-		for (final EMFFormsDMRExpander dmrExpander : emfFormsDMRExpanders) {
-			if (dmrExpander.isApplicable(domainModelReference) > bestScore) {
-				bestScore = dmrExpander.isApplicable(domainModelReference);
-				bestDMRExpander = dmrExpander;
+		final EList<VDomainModelReferenceSegment> segments = domainModelReference.getSegments();
+		if (segments.isEmpty()) {
+			// TODO ?throw Exception?
+			return;
+		}
+
+		EObject currentDomainObject = domainObject;
+		// the last segment is not expanded
+		for (int i = 0; i < segments.size() - 1; i++) {
+			final VDomainModelReferenceSegment segment = segments.get(i);
+			final EMFFormsDMRSegmentExpander expander = getBestSegmentExpander(segment);
+			currentDomainObject = expander.prepareDomainObject(segment, currentDomainObject);
+		}
+	}
+
+	/**
+	 *
+	 * @param segment The {@link VDomainModelReferenceSegment} to get the best expander for
+	 * @return the most suitable {@link EMFFormsDMRSegmentExpander} for the given segment
+	 * @throws EMFFormsExpandingFailedException if no {@link EMFFormsDMRSegmentExpander} could be found
+	 */
+	private EMFFormsDMRSegmentExpander getBestSegmentExpander(VDomainModelReferenceSegment segment)
+		throws EMFFormsExpandingFailedException {
+		EMFFormsDMRSegmentExpander bestSegmentExpander = null;
+		double bestScore = EMFFormsDMRSegmentExpander.NOT_APPLICABLE;
+		for (final EMFFormsDMRSegmentExpander expander : emfFormsDMRSegmentExpanders) {
+			if (expander.isApplicable(segment) > bestScore) {
+				bestScore = expander.isApplicable(segment);
+				bestSegmentExpander = expander;
 			}
 		}
 
-		if (bestDMRExpander == null) {
+		if (bestSegmentExpander == null) {
 			throw new EMFFormsExpandingFailedException(
-				"There is no suitable EMFFormsDMRExpander for the given domain model reference."); //$NON-NLS-1$
+				String.format("There is no suitable EMFFormsDMRSegmentExpander for the given segment %1$s.", segment)); //$NON-NLS-1$
 		}
 
-		bestDMRExpander.prepareDomainObject(domainModelReference, domainObject);
+		return bestSegmentExpander;
 	}
 }
