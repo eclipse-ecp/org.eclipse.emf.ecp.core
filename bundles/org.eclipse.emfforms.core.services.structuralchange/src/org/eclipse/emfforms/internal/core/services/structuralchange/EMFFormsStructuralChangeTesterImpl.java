bc399c6390a8b839200afb33e17ebb0e4ffd47d6
Lucas Koehler <l.koehler93@gmail.com>
2016-06-30 12:49:11 +0200

    Bug 462873 - Change DMR to use segments
    EMFFormsStructuralChangeTesterImpl and feature resolver
    * adjusted EMFFormsStructuralChangeTesterImpl to segments: it now uses
    the newly defined StructuralChangeSegmentResolvers to resolve segments
    to Settings.
    * defined StructuralChangeSegmentResolver interface
    * implemented a StructuralChangeSegmentResolvers for feature segments
    
    Change-Id: I7dbb0186978e65cc33aa333b628700882e32bee8
    Signed-off-by: Lucas Koehler <l.koehler93@gmail.com>

--- bundles/org.eclipse.emfforms.core.services.structuralchange/src/org/eclipse/emfforms/internal/core/services/structuralchange/EMFFormsStructuralChangeTesterImpl.java
+++ bundles/org.eclipse.emfforms.core.services.structuralchange/src/org/eclipse/emfforms/internal/core/services/structuralchange/EMFFormsStructuralChangeTesterImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ * Copyright (c) 2011-2016 EclipseSource Muenchen GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,19 +8,26 @@
  *
  * Contributors:
  * Lucas Koehler - initial API and implementation
+ * Lucas Koehler - adjust to DMR segments
  ******************************************************************************/
 package org.eclipse.emfforms.internal.core.services.structuralchange;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
+import org.eclipse.emf.ecp.common.spi.asserts.Assert;
 import org.eclipse.emf.ecp.view.spi.model.ModelChangeNotification;
 import org.eclipse.emf.ecp.view.spi.model.VDomainModelReference;
+import org.eclipse.emf.ecp.view.spi.model.VDomainModelReferenceSegment;
 import org.eclipse.emfforms.spi.common.report.AbstractReport;
 import org.eclipse.emfforms.spi.common.report.ReportService;
 import org.eclipse.emfforms.spi.core.services.structuralchange.EMFFormsStructuralChangeTester;
-import org.eclipse.emfforms.spi.core.services.structuralchange.StructuralChangeTesterInternal;
+import org.eclipse.emfforms.spi.core.services.structuralchange.StructuralChangeSegmentResolver;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
@@ -36,7 +43,7 @@ import org.osgi.service.component.annotations.ReferencePolicy;
 public class EMFFormsStructuralChangeTesterImpl implements EMFFormsStructuralChangeTester {
 
 	private ReportService reportService;
-	private final Set<StructuralChangeTesterInternal> structuralChangeTesters = new LinkedHashSet<StructuralChangeTesterInternal>();
+	private final Set<StructuralChangeSegmentResolver> structuralChangeResolvers = new LinkedHashSet<StructuralChangeSegmentResolver>();
 
 	/**
 	 * Sets the {@link ReportService}.
@@ -49,22 +56,22 @@ public class EMFFormsStructuralChangeTesterImpl implements EMFFormsStructuralCha
 	}
 
 	/**
-	 * Called by the framework to add a {@link StructuralChangeTesterInternal}.
+	 * Called by the framework to add a {@link StructuralChangeSegmentResolver}.
 	 *
-	 * @param structuralChangeTester The {@link StructuralChangeTesterInternal} to add
+	 * @param structuralChangeResolver The {@link StructuralChangeSegmentResolver} to add
 	 */
 	@Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
-	protected void addStructuralChangeTesterInternal(StructuralChangeTesterInternal structuralChangeTester) {
-		structuralChangeTesters.add(structuralChangeTester);
+	protected void addStructuralChangeSegmentResolver(StructuralChangeSegmentResolver structuralChangeResolver) {
+		structuralChangeResolvers.add(structuralChangeResolver);
 	}
 
 	/**
-	 * Called by the framework to remove a {@link StructuralChangeTesterInternal}.
+	 * Called by the framework to remove a {@link StructuralChangeSegmentResolver}.
 	 *
-	 * @param structuralChangeTester The {@link StructuralChangeTesterInternal} to remove
+	 * @param structuralChangeResolver The {@link StructuralChangeSegmentResolver} to remove
 	 */
-	protected void removeStructuralChangeTesterInternal(StructuralChangeTesterInternal structuralChangeTester) {
-		structuralChangeTesters.remove(structuralChangeTester);
+	protected void removeStructuralChangeSegmentResolver(StructuralChangeSegmentResolver structuralChangeResolver) {
+		structuralChangeResolvers.remove(structuralChangeResolver);
 	}
 
 	/**
@@ -76,23 +83,78 @@ public class EMFFormsStructuralChangeTesterImpl implements EMFFormsStructuralCha
 	@Override
 	public boolean isStructureChanged(VDomainModelReference reference, EObject domainRootObject,
 		ModelChangeNotification notification) {
-		double bestPriority = StructuralChangeTesterInternal.NOT_APPLICABLE;
-		StructuralChangeTesterInternal bestTester = null;
-		for (final StructuralChangeTesterInternal tester : structuralChangeTesters) {
-			final double priority = tester.isApplicable(reference);
-			if (priority > bestPriority) {
-				bestPriority = priority;
-				bestTester = tester;
-			}
-		}
+		Assert.create(reference).notNull();
+		Assert.create(domainRootObject).notNull();
+		Assert.create(notification).notNull();
 
-		if (bestTester == null) {
-			reportService.report(new AbstractReport("Warning: Structural changes of the DMR: " + reference //$NON-NLS-1$
-				+ "could not be analyzed because no suitable StructuralChangeTesterInternal was available.")); //$NON-NLS-1$
+		// Changed EAttributes or only touched EReferences do not constitute structural changes
+		if (EAttribute.class.isInstance(notification.getStructuralFeature())) {
+			return false;
+		}
+		if (notification.getRawNotification().isTouch()) {
 			return false;
 		}
 
-		return bestTester.isStructureChanged(reference, domainRootObject, notification);
+		final EList<VDomainModelReferenceSegment> segments = reference.getSegments();
+
+		boolean relevantChange = false;
+
+		EObject currentDomainObject = domainRootObject;
+		for (final VDomainModelReferenceSegment segment : segments) {
+			final StructuralChangeSegmentResolver segmentResolver = getBestSegmentResolver(segment);
+			if (segmentResolver == null) {
+				reportService.report(new AbstractReport("Warning: Structural changes of the DMR: " + reference //$NON-NLS-1$
+					+ "could not be analyzed because no suitable StructuralChangeSegmentResolver was available.")); //$NON-NLS-1$
+				return false;
+			}
+
+			/*
+			 * Check whether the notifying EObject and the EReference match the notification.
+			 * If the EStructuralFeature of the resolved Setting is an EAttribute, we cannot resolve further and its
+			 * change is irrelevant.
+			 */
+			final Setting setting = segmentResolver.resolveSegment(segment, currentDomainObject);
+			if (setting == null) {
+				break;
+			}
+
+			if (EReference.class.isInstance(setting.getEStructuralFeature())) {
+				final EReference eReference = (EReference) setting.getEStructuralFeature();
+				relevantChange |= eReference.equals(notification.getStructuralFeature())
+					&& notification.getNotifier() == setting.getEObject();
+			} else {
+				break;
+			}
+
+			if (relevantChange) {
+				return true;
+			}
+
+			// The value of the Setting is an EObject because its EStructuralFeature is an EReference.
+			currentDomainObject = (EObject) setting.get(true);
+		}
+
+		return relevantChange;
+
+	}
+
+	/**
+	 * @param segment
+	 * @return The most suitable {@link StructuralChangeSegmentResolver} for the given
+	 *         {@link VDomainModelReferenceSegment segment}, <code>null</code> if there is none
+	 */
+	private StructuralChangeSegmentResolver getBestSegmentResolver(VDomainModelReferenceSegment segment) {
+		double bestPriority = StructuralChangeSegmentResolver.NOT_APPLICABLE;
+		StructuralChangeSegmentResolver bestTester = null;
+
+		for (final StructuralChangeSegmentResolver resolver : structuralChangeResolvers) {
+			final double priority = resolver.isApplicable(segment);
+			if (priority > bestPriority) {
+				bestPriority = priority;
+				bestTester = resolver;
+			}
+		}
+		return bestTester;
 	}
 
 }
