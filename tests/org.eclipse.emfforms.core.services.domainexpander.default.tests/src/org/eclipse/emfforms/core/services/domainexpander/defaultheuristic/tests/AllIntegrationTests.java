569b0576f31c5d79edfa4261f1d5fee07742fdcd
Lucas Koehler <l.koehler93@gmail.com>
2016-09-13 17:36:39 +0200

    Bug 462873 - Change DMR to use segments
    Tests for core.services.domainexpander.default:
    * Tests and ITests for EMFFormsDMRSegmentExpanderFeature
    * adapted tests and itests of EMFFormsDomainExpanderImpl to segments
    * extended test model for the tests
    * removed obsolete tests
    
    Change-Id: Iec02b937815e073908143ccfc0cdd5689bd8b60c
    Signed-off-by: Lucas Koehler <l.koehler93@gmail.com>

--- tests/org.eclipse.emfforms.core.services.domainexpander.default.tests/src/org/eclipse/emfforms/core/services/domainexpander/defaultheuristic/tests/AllIntegrationTests.java
+++ tests/org.eclipse.emfforms.core.services.domainexpander.default.tests/src/org/eclipse/emfforms/core/services/domainexpander/defaultheuristic/tests/AllIntegrationTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ * Copyright (c) 2011-2016 EclipseSource Muenchen GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,7 +11,7 @@
  ******************************************************************************/
 package org.eclipse.emfforms.core.services.domainexpander.defaultheuristic.tests;
 
-import org.eclipse.emfforms.internal.core.services.domainexpander.defaultheuristic.EMFFormsDMRExpanderDefaultHeuristic_ITest;
+import org.eclipse.emfforms.internal.core.services.domainexpander.defaultheuristic.EMFFormsDMRSegmentExpanderFeature_ITest;
 import org.eclipse.emfforms.internal.core.services.domainexpander.defaultheuristic.EMFFormsDomainExpanderImpl_ITest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -24,7 +24,7 @@ import org.junit.runners.Suite.SuiteClasses;
  *
  */
 @RunWith(Suite.class)
-@SuiteClasses({ EMFFormsDMRExpanderDefaultHeuristic_ITest.class, EMFFormsDomainExpanderImpl_ITest.class })
+@SuiteClasses({ EMFFormsDMRSegmentExpanderFeature_ITest.class, EMFFormsDomainExpanderImpl_ITest.class })
 public class AllIntegrationTests {
 
 }
