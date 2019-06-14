90a943084a80032cb0864034d02ed1de9ec8c6ee
Eugen Neufeld <eneufeld@eclipsesource.com>
2019-06-14 17:56:38 +0200

    Bug 541605 - [View Editor] Generate controls - Different behavior for abstract and concrete classes
    Removed EClass selection as we support View Models for Abstract classes.
    
    Change-Id: I495aafcbd26a8eeecf3fc231a1fb3b94e1ed40de
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

--- bundles/org.eclipse.emf.ecp.ui.view.editor.controls/src/org/eclipse/emf/ecp/view/internal/editor/handler/SelectAttributesWizard.java
+++ bundles/org.eclipse.emf.ecp.ui.view.editor.controls/src/org/eclipse/emf/ecp/view/internal/editor/handler/SelectAttributesWizard.java
@@ -18,7 +18,6 @@ import java.util.Set;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecp.view.spi.model.VView;
-import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 
 /**
@@ -27,69 +26,39 @@ import org.eclipse.jface.wizard.Wizard;
  */
 public class SelectAttributesWizard extends Wizard {
 
-	private SelectDataSegmentWizardPage selectDataSegmentWizardPage;
 	private SelectAttributesWizardPage selectAttributesWizardPage;
-	private EClass rootClass;
-	private VView view;
+	private final EClass rootClass;
+	private final VView view;
 	private Set<EStructuralFeature> selectedFeatures;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param view The {@link VView} the Controls are generated for.
+	 */
+	public SelectAttributesWizard(VView view) {
+		this.view = view;
+		rootClass = view.getRootEClass();
+	}
 
 	@Override
 	public void addPages() {
-		selectDataSegmentWizardPage = new SelectDataSegmentWizardPage();
-		selectDataSegmentWizardPage.setTitle("Select Data Segment"); //$NON-NLS-1$
-		selectDataSegmentWizardPage.setDescription("Select an EClass."); //$NON-NLS-1$
-		selectDataSegmentWizardPage.setView(view);
-		addPage(selectDataSegmentWizardPage);
-
 		selectAttributesWizardPage = new SelectAttributesWizardPage();
 		selectAttributesWizardPage
 			.setDescription("Select the attributes for which controls should be generated."); //$NON-NLS-1$
 		selectAttributesWizardPage.setTitle("Select Attributes"); //$NON-NLS-1$
+
+		selectAttributesWizardPage.setRootClass(rootClass);
+		selectAttributesWizardPage.setView(view);
 		addPage(selectAttributesWizardPage);
 
 	}
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see org.eclipse.jface.wizard.Wizard#getNextPage(org.eclipse.jface.wizard.IWizardPage)
-	 */
-	@Override
-	public IWizardPage getNextPage(IWizardPage page) {
-		if (SelectDataSegmentWizardPage.class.isInstance(page)) {
-			final SelectDataSegmentWizardPage selectPage = (SelectDataSegmentWizardPage) page;
-			if (selectPage != null) {
-				if (selectAttributesWizardPage == null) {
-					selectAttributesWizardPage = new SelectAttributesWizardPage();
-					selectAttributesWizardPage
-						.setDescription("Select the attributes for which controls should be generated."); //$NON-NLS-1$
-					selectAttributesWizardPage.setTitle("Select Attributes"); //$NON-NLS-1$
-					addPage(selectAttributesWizardPage);
-				}
-				rootClass = selectPage.getSelectedDataSegment();
-				selectAttributesWizardPage.setRootClass(rootClass);
-				selectAttributesWizardPage.setView(view);
-			}
-			return selectAttributesWizardPage;
-		}
-		return null;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see org.eclipse.jface.wizard.Wizard#canFinish()
-	 */
 	@Override
 	public boolean canFinish() {
-		return selectAttributesWizardPage != null && !selectAttributesWizardPage.getSelectedFeatures().isEmpty();
+		return !selectAttributesWizardPage.getSelectedFeatures().isEmpty();
 	}
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
-	 */
 	@Override
 	public boolean performFinish() {
 		selectedFeatures = selectAttributesWizardPage.getSelectedFeatures();
@@ -102,38 +71,5 @@ public class SelectAttributesWizard extends Wizard {
 	 */
 	public Set<EStructuralFeature> getSelectedFeatures() {
 		return selectedFeatures;
-	}
-
-	/**
-	 * @param rootClass the {@link EClass} to select attributes from
-	 */
-	protected void setRootEClass(EClass rootClass) {
-		this.rootClass = rootClass;
-
-	}
-
-	/**
-	 * @return rootClass
-	 */
-	protected EClass getRootEClass() {
-		return rootClass;
-
-	}
-
-	/**
-	 * @param view The VView to use
-	 */
-	protected void setView(VView view) {
-		this.view = view;
-
-	}
-
-	/**
-	 *
-	 */
-	public void backPressed() {
-		if (selectAttributesWizardPage != null && selectAttributesWizardPage.isCurrentPage()) {
-			selectAttributesWizardPage.clearSelection();
-		}
 	}
 }
