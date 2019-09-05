3d4abfb781a687690d8d9070acc323b12b88360f
Eugen Neufeld <eneufeld@eclipsesource.com>
2019-09-05 12:51:08 +0200

    Bug 550799 - Improve DataTemplate Editor reference handling
    * Add a DataTemplate specific CreateNewModelElementStrategy which
    forbids to add objects for non containment references
    * Add a DataTemplate specific EObjectSelectionStrategy which only allows
    objects from the same template
    * Add RCPTT smoke test
    
    Change-Id: I057c30f842d9a80fb1eed6b999312fd524d4c260
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

---
+++ bundles/org.eclipse.emfforms.datatemplate.tooling/src/org/eclipse/emfforms/internal/datatemplate/tooling/editor/Messages.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2019 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * eugen - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emfforms.internal.datatemplate.tooling.editor;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Eugen Neufeld
+ * @generated
+ */
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.emfforms.internal.datatemplate.tooling.editor.messages"; //$NON-NLS-1$
+	public static String DataTemplateCreateNewModelElement_0;
+	public static String DataTemplateCreateNewModelElement_1;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
