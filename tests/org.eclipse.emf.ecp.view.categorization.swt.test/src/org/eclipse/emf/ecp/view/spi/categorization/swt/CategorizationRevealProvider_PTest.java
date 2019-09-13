ca07c452a9a5ea0f849a04412b78dc9282bead30
Christian W. Damus <give.a.damus@gmail.com>
2019-09-13 17:09:13 -0400

    Bug 551066 - [Reveal Service] Revealing a control does not work within a category
    Defer finding the renderer of the control because in the case of a
    control within a category view it will not be rendered until the
    category itself is first revealed by a prior reveal step.
    
    Change-Id: Id0a7c876a08d7dbc706f2ee41fecc039897d7561
    Signed-off-by: Christian W. Damus <give.a.damus@gmail.com>

--- tests/org.eclipse.emf.ecp.view.categorization.swt.test/src/org/eclipse/emf/ecp/view/spi/categorization/swt/CategorizationRevealProvider_PTest.java
+++ tests/org.eclipse.emf.ecp.view.categorization.swt.test/src/org/eclipse/emf/ecp/view/spi/categorization/swt/CategorizationRevealProvider_PTest.java
@@ -13,16 +13,21 @@
  ******************************************************************************/
 package org.eclipse.emf.ecp.view.spi.categorization.swt;
 
+import static org.eclipse.emf.ecp.common.spi.UniqueSetting.createSetting;
 import static org.eclipse.emf.ecp.view.test.common.spi.EMFMocking.eMock;
 import static org.eclipse.emf.ecp.view.test.common.spi.EMFMocking.withESettings;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecp.test.common.DefaultRealm;
 import org.eclipse.emf.ecp.view.internal.categorization.swt.CategorizationRevealProvider;
 import org.eclipse.emf.ecp.view.spi.categorization.model.VCategorization;
@@ -32,7 +37,11 @@ import org.eclipse.emf.ecp.view.spi.categorization.model.VCategory;
 import org.eclipse.emf.ecp.view.spi.context.ViewModelContext;
 import org.eclipse.emf.ecp.view.spi.model.VContainedContainer;
 import org.eclipse.emf.ecp.view.spi.model.VContainer;
+import org.eclipse.emf.ecp.view.spi.model.VControl;
 import org.eclipse.emf.ecp.view.spi.model.VElement;
+import org.eclipse.emf.ecp.view.spi.model.VViewFactory;
+import org.eclipse.emf.ecp.view.spi.vertical.model.VVerticalFactory;
+import org.eclipse.emf.ecp.view.spi.vertical.model.VVerticalLayout;
 import org.eclipse.emf.ecp.view.test.common.spi.EMFFormsRevealServiceFixture;
 import org.eclipse.emf.ecp.view.test.common.spi.EMFFormsViewContextFixture.DomainModel;
 import org.eclipse.emf.ecp.view.test.common.spi.EMFFormsViewContextFixture.ViewModel;
@@ -41,6 +50,7 @@ import org.eclipse.emf.ecp.view.test.common.spi.EMock;
 import org.eclipse.emf.ecp.view.test.common.swt.spi.SWTViewTestHelper;
 import org.eclipse.emfforms.bazaar.Bid;
 import org.eclipse.emfforms.bazaar.Create;
+import org.eclipse.emfforms.spi.core.services.controlmapper.EMFFormsSettingToControlMapper;
 import org.eclipse.emfforms.spi.core.services.reveal.EMFFormsRevealProvider;
 import org.eclipse.emfforms.spi.core.services.reveal.RevealStep;
 import org.eclipse.swt.widgets.Shell;
@@ -61,9 +71,8 @@ public class CategorizationRevealProvider_PTest {
 	private final VCategorizationElement categorizations = VCategorizationFactory.eINSTANCE
 		.createCategorizationElement();
 
-	@EMock
 	@DomainModel
-	private EObject rootObject;
+	private final EObject rootObject = EcoreFactory.eINSTANCE.createEClass();
 
 	@EMock
 	private EObject obj1;
@@ -190,6 +199,40 @@ public class CategorizationRevealProvider_PTest {
 		assertThat("The category was not selected", categorizations.getCurrentSelection(), is(cat2));
 
 		verify(reveal).run();
+	}
+
+	/**
+	 * Integration test for revealing a specific control (by feature) within a tree categorization,
+	 * which is the scenario that didn't work as reported in <a href="http://eclip.se/551066">bug 551066</a>.
+	 * The test relies on the fact that the non-default category containing the control to be revealed
+	 * would not end up being selected if the control within did not provide a successful reveal step.
+	 *
+	 * @see <a href="http://eclip.se/551066">bug 551066</a>
+	 */
+	@Test
+	public void revealSpecificControlInTree() {
+		final VVerticalLayout vertical = VVerticalFactory.eINSTANCE.createVerticalLayout();
+		cat3.setComposite(vertical);
+
+		final VControl control = VViewFactory.eINSTANCE.createControl();
+		final EStructuralFeature feature = EcorePackage.Literals.ENAMED_ELEMENT__NAME;
+		control.setDomainModelReference(feature);
+		vertical.getChildren().add(control);
+
+		// Revealing a control requires the settings mapper
+		final EMFFormsSettingToControlMapper mapper = mock(EMFFormsSettingToControlMapper.class);
+		when(mapper.hasControlsFor(rootObject)).thenReturn(true);
+		when(mapper.hasMapping(createSetting(rootObject, feature), control)).thenReturn(true);
+		fixture.putService(EMFFormsSettingToControlMapper.class, mapper);
+
+		render();
+
+		fixture.reveal(rootObject, feature);
+
+		// The category would only be selected if the control within it was revealed.
+		// In the case of bug 551066, context injection of the ControlRevealProvider failed
+		// to create the provider because the name control renderer was not yet available
+		assertThat("The category was not selected", categorizations.getCurrentSelection(), is(cat3));
 	}
 
 	//
